ec81fd5544257b3c384b2cc577af7574700c2953
Seme30 <semahegn.adugna1@gmail.com>
2024-03-29 23:01:47 +0300

    changed the look of login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,35 +1,31 @@
 <template>
-    <v-container>
-      <v-slide-y-transition mode="out-in">
-          <v-layout row align-center>
-            <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg4 offset-lg4>
-              <v-card style="margin-bottom:200px;">
-                <v-card-title primary-title>
-                    <div class="pa-3">
-                    LOGO GOES HERE
-                </div>
-                  <div>
-                    <h3 class="headline mb-0">Welcome</h3>
-                    <div>Welcome to My Application.                          
-                      <br><br>
-                      Login to connect. 
-                      <br><br>
-
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-
-                    </div>
-                  </div>
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn  @click="auth0Login()">Login</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
-
+  <v-container>
+    <v-slide-y-transition mode="out-in">
+      <v-row>
+        <v-col cols="12"  offset-sm="2" md="6" offset-md="3" lg="4" offset-lg="4">
+          <br><br>
+          <h3 class="headline mb-0">Welcome</h3>
+          <br><br>
+          <v-card 
+           title="Welcome to My Application."
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                "
+            variant="tonal" style="margin-bottom:200px;">
+            <v-card-title primary-title>
+              
+            </v-card-title>
+            <v-card-actions style="margin: auto;">
+              <v-btn  @click="auth0Login()">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-slide-y-transition>
+  </v-container>
 </template>
+
 
 <script>
 import { defineComponent } from 'vue';
